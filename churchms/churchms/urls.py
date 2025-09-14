4388c0f290062b9426448109b2963e5c11d47dce
Wonahh <wonahgodwin40@gmail.com>
2025-09-14 23:16:06 +0100

    omo

--- churchms/churchms/urls.py
+++ churchms/churchms/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include--
 from django.http import JsonResponse
 from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.contrib.auth import views as auth_views
@@ -33,5 +33,4 @@ urlpatterns = [
     path('api/', include('home.api_urls')),
     path("logout/", auth_views.LogoutView.as_view(next_page="https://churchms-site.webflow.io/#learn-more"), name="logout"),
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    
 ]
