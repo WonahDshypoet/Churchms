265eed5d9f3d299edfcac1099473442331852612
Wonahh <wonahgodwin40@gmail.com>
2025-09-17 14:02:41 +0100

    created admin user

--- churchms/home/admin.py
+++ churchms/home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Family, Member, Event, Registration, Donation, Communication, Devotional, CustomUser
+
+admin.site.register(CustomUser)
+admin.site.register(Family)
+admin.site.register(Member)
+admin.site.register(Event)
+admin.site.register(Registration)
+admin.site.register(Donation)
+admin.site.register(Communication)
+admin.site.register(Devotional)
